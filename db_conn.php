64a91425aac40dcd021ecd099cdbb9e7f01bae29
Oxa <96934856+oxcilin@users.noreply.github.com>
2024-05-05 08:47:12 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -2,9 +2,9 @@
 
 // Database configuration
 $servername = "localhost";  // Replace with your server name
-$username = "root";         // Replace with your database username
+$username = "";         // Replace with your database username
 $password = "";     // Replace with your database password
-$dbname = "db_oxa";       // Replace with your database name
+$dbname = "";       // Replace with your database name
 
 // Create a connection to the database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
